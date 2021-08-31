841d3f392bf4c5d6d016f8528d0613c62fce7b8a
ArthurFerreiraRodrigues <69909375+ArthurFerreiraRodrigues@users.noreply.github.com>
2021-08-31 04:04:45 -0300

    Manager login system created

--- TP3/src/model/Manager.java
+++ TP3/src/model/Manager.java
@@ -1,5 +1,75 @@
 package model;
 
-public class Manager {
+import util.Print;
+import util.Read;
 
+public class Manager {
+    private String name, username, password;
+    boolean loginStatus;
+
+    /**
+     * Gera o profile do Gerente
+     * 
+     * @param name     : nome do Gerente
+     * @param username : usuario de login
+     * @param password : senha de login
+     */
+    public Manager(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    private String getUsername() {
+        return this.username;
+    }
+
+    private String getPassword() {
+        return this.password;
+    }
+
+    public boolean getLoginStatus() {
+        return this.loginStatus;
+    }
+
+    /*----------------------------------------------------------------------*/
+
+    private static Manager verifyLogin(String inputUser, String inputPass) {
+
+        for (Manager manager : Data.getManagers()) {
+            if (manager.getUsername().equals(inputUser)) {
+                if (manager.getPassword().equals(inputPass)) {
+                    return manager;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void login() {
+        String inputUser, inputPass;
+        Manager loginModel;
+
+        Print.titleAndDescription("Login de Gerente", "Usuario e Senha padrao : Gerente");
+        do {
+            System.out.printf("Usuario : ");
+            inputUser = Read.Line();
+            System.out.printf("Senha : ");
+            inputPass = Read.Line();
+
+            loginModel = verifyLogin(inputUser, inputPass);
+            if (loginModel == null) {
+                System.out.printf("\nUsuario ou Senha sem correspondentes. Tente novamente.");
+            }
+            Print.split();
+        } while (loginModel == null);
+        loginModel.loginStatus = true;
+
+        System.out.printf("Login realizado com sucesso !");
+    }
 }
