fab3c22aa8046516ecdc50b76a295549957755c7
ArthurFerreiraRodrigues <69909375+ArthurFerreiraRodrigues@users.noreply.github.com>
2021-09-08 02:24:15 -0300

    Extend methods

---
+++ TP3/src/products/Miscellaneous.java
@@ -0,0 +1,35 @@
+package products;
+
+import model.Product;
+
+public class Miscellaneous extends Product {
+
+    public Miscellaneous(int id, double price, int inStockQuant) {
+        super(id, price, inStockQuant);
+
+    }
+
+    @Override
+    public int getId() {
+
+        return 0;
+    }
+
+    @Override
+    public double getPrice() {
+
+        return 0;
+    }
+
+    @Override
+    public int getInStockQuant() {
+
+        return 0;
+    }
+
+    @Override
+    public void uptateStockQuant(int quantSold) {
+
+    }
+
+}
