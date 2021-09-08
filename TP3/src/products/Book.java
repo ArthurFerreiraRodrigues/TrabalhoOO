76e49326fb9fc80e15a598477d484b509eb8c1a3
ArthurFerreiraRodrigues <69909375+ArthurFerreiraRodrigues@users.noreply.github.com>
2021-09-08 02:54:06 -0300

    Atributes added

--- TP3/src/products/Book.java
+++ TP3/src/products/Book.java
@@ -3,10 +3,16 @@ package products;
 import model.Product;
 
 public class Book extends Product {
+    private String author, title, type, genre;
 
-    public Book(int id, double price, int inStockQuant) {
+    // Costructor
+
+    public Book(int id, double price, int inStockQuant, String author, String title, String type, String genre) {
         super(id, price, inStockQuant);
-
+        this.author = author;
+        this.title = title;
+        this.type = type;
+        this.genre = genre;
     }
 
     @Override
--- TP3/src/products/Miscellaneous.java
+++ TP3/src/products/Miscellaneous.java
@@ -3,10 +3,14 @@ package products;
 import model.Product;
 
 public class Miscellaneous extends Product {
+    private String name, brand;
 
-    public Miscellaneous(int id, double price, int inStockQuant) {
+    // Constructor
+
+    public Miscellaneous(int id, double price, int inStockQuant, String name, String brand) {
         super(id, price, inStockQuant);
-
+        this.name = name;
+        this.brand = brand;
     }
 
     @Override
