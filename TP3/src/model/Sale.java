eff84ac6dc88817b1eb9ad77da7d94c71c6bccdb
ArthurFerreiraRodrigues <69909375+ArthurFerreiraRodrigues@users.noreply.github.com>
2021-09-09 02:24:40 -0300

    Base Sale class structure stablished

--- TP3/src/model/Sale.java
+++ TP3/src/model/Sale.java
@@ -1,5 +1,49 @@
 package model;
 
-public class Sale {
+import java.util.ArrayList;
 
+public class Sale {
+    private Seller seller;
+    private Customer buyer;
+    private ArrayList<Product> productsSold = new ArrayList<>();
+    private int unitsSold;
+    private double subTotal;
+
+    // Constructor
+
+    public Sale(Seller seller, Customer buyer, ArrayList<Product> productsSold, int unitsSold, double subTotal) {
+        this.seller = seller;
+        this.buyer = buyer;
+        this.productsSold = productsSold;
+        this.unitsSold = unitsSold;
+        this.subTotal = subTotal;
+    }
+
+    // Getters
+
+    public Seller getseller() {
+        return seller;
+    }
+
+    public Customer getbuyer() {
+        return buyer;
+    }
+
+    public ArrayList<Product> getproductsSold() {
+        return productsSold;
+    }
+
+    public int getunitsSold() {
+        return unitsSold;
+    }
+
+    public double getSubTotal() {
+        return subTotal;
+    }
+
+    // Lister
+
+    public void list() {
+
+    }
 }
