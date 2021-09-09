5d4b98873fae74743d9fc774ab808f8c2f3bef19
ArthurFerreiraRodrigues <69909375+ArthurFerreiraRodrigues@users.noreply.github.com>
2021-09-09 09:56:39 -0300

    Seller deleter added

--- TP3/src/model/StoreManager.java
+++ TP3/src/model/StoreManager.java
@@ -473,6 +473,46 @@ public class StoreManager {
 
     /*----------------------------------------------------------------------*/
     /**
+     * Deletes Seller.
+     */
+    public void deleteSeller() {
+        int searchId;
+        Seller match;
+        int confirmEdit;
+
+        Print.titleAndDescription("Deletar Dados de Vendedor", "ID | Nome");
+
+        System.out.printf("Buscar Vendedor (ID) : ");
+        searchId = Read.Int();
+        match = Seller.searchById(searchId);
+
+        if (match == null) {
+            System.out.printf("\nDeletar não encontrado!");
+        } else {
+
+            do {
+                System.out.printf("\nDeletar Dados do Vendedor ID : %d ?\n\t.1 - Sim\n\t.2 - Não, voltar ao menu",
+                        match.id);
+                System.out.printf("\n\nEscolha : ");
+                confirmEdit = Read.Int();
+                switch (confirmEdit) {
+                    case 1:
+                        Data.getSellers().remove(match);
+                        break;
+                    case 2:
+                        System.out.println("Voltando ao menu.");
+                        break;
+                    default:
+                        System.out.printf("\nDigite uma opção válida.");
+                        break;
+                }
+            } while (confirmEdit != 1 && confirmEdit != 2);
+
+        }
+    }
+
+    /*----------------------------------------------------------------------*/
+    /**
      * Deletes StoreManager.
      */
     public void deleteStoreManager() {
