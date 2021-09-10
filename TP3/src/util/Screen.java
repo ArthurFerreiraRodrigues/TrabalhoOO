e053abdedb12b3fee49f538287fb3b8ccab6926f
ArthurFerreiraRodrigues <69909375+ArthurFerreiraRodrigues@users.noreply.github.com>
2021-09-10 09:54:53 -0300

    pressEnter method added

--- TP3/src/util/Screen.java
+++ TP3/src/util/Screen.java
@@ -4,4 +4,11 @@ public class Screen {
     public static void clear() {
         System.out.print("\033\143");
     }
+
+    public static void pressEnter() {
+        Print.split();
+        System.out.printf("Aperte ENTER para continuar.");
+        Print.split();
+        Read.Line();
+    }
 }
