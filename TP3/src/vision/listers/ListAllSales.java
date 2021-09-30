70bcef0485276830d372883adfc4a0b29eb508b6
ArthurFerreiraRodrigues <69909375+ArthurFerreiraRodrigues@users.noreply.github.com>
2021-09-29 21:16:12 -0300

    ListSalesMenu options created

---
+++ TP3/src/vision/listers/ListAllSales.java
@@ -0,0 +1,29 @@
+package vision.listers;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import control.InicialMenuControl;
+
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class ListAllSales extends JPanel implements ActionListener {
+
+    public ListAllSales() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
